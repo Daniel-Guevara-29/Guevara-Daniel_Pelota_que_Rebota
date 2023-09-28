36f1e93878bf2c6aaf74d4255c3e935efe361dfb
daniel <daniel.guevara@ucuenca.edu.ec>
2023-09-28 16:51:12 -0500

    updates

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+let posX, posY, diam, 
+let rad; 
+esp;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  posX = windowWidth / 2;
+    posY = windowHeight / 2;
+    rectMode(CENTER);
+    piso = windowHeight * margen * esp / 2;
+    
+
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  background(0, 200, 150, 70);
+  noStroke();
+
+  if(posX){
+
+  }
+
+    fill(255)
+    circle(posX, posY, 50);
+  
+  
+    fill(255, 0, 0);
+    rect(windowWidth * 0.5, windowHeight - 50, windowWidth * 0.8, 20);
+    noStroke(); 
+
 }

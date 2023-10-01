f63ec269af56ab14ec3cba43938df9d273ee21a1
daniel <daniel.guevara@ucuenca.edu.ec>
2023-10-01 12:49:48 -0500

    se estableció rebote sucesivo y se aumento el diámetro

--- sketch.js
+++ sketch.js
@@ -1,30 +1,48 @@
-let posX, posY, diam, 
-let rad; 
-esp;
+let posX, posY, diam, esp, piso, rad;
+let velocidadY = 0;
+let gravedad = 0.5;
+let rebote = false;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   posX = windowWidth / 2;
     posY = windowHeight / 2;
     rectMode(CENTER);
-    piso = windowHeight * margen * esp / 2;
-    
-
+    diam = 70;
+    rad = diam / 2; 
+    esp = 0.8; 
+    piso = windowHeight * 0.9;
+    posX = windowWidth / 2;
+  posY = windowHeight / 2;
 }
 
 function draw() {
   background(0, 200, 150, 70);
   noStroke();
 
-  if(posX){
+  velocidadY += gravedad;
+  posY += velocidadY;
 
+  if (posY + rad >= piso) {
+    posY = piso - rad;
+    velocidadY *= -esp;
+    rebote = true;
+  } else {
+    rebote = false;
   }
 
+
     fill(255)
-    circle(posX, posY, 50);
+    circle(posX, posY, diam);
   
   
     fill(255, 0, 0);
-    rect(windowWidth * 0.5, windowHeight - 50, windowWidth * 0.8, 20);
-    noStroke(); 
+    rect(windowWidth * 0.5, piso, windowWidth, 20);
 
-}
+    if (rebote && posY >= piso - rad && velocidadY >= -1 && velocidadY <= 1) {
+      posY = -rad;
+      velocidadY = 0;
+      rebote = false;
+    }
+  }
+
